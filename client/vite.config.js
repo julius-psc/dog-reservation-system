6cb68eed691a5d0d8a492f70c53684a82da0f849
Julius Peschard <juliuspeschard@MacBook-Air-de-Julius.local>
2025-02-20 22:48:40 +0100

    Update routing with vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+// Derive __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
+  build: {
+    outDir: path.resolve(__dirname, '../server/dist'), // Output to server/dist
+    emptyOutDir: true // Clear the directory before building
+  }
 });
