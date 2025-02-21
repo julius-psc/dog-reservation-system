53a3b1325211bf3432dac078c067973d5f045d8a
Julius Peschard <juliuspeschard@MacBook-Air-de-Julius.local>
2025-02-21 14:03:59 +0100

    Fix vite config and change build command

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // Ensure this matches your deployment URL root
-});
+  base: '/'
+})
