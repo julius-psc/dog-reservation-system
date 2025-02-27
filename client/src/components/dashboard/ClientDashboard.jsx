fdcb9407acfeaf1eceb816bbdaff64c07de24cff
Julius Peschard <juliuspeschard@MacBook-Air-de-Julius.local>
2025-02-27 20:36:14 +0100

    Translate client dashboard dog profile

--- client/src/components/dashboard/ClientDashboard.jsx
+++ client/src/components/dashboard/ClientDashboard.jsx
@@ -500,7 +500,7 @@ const ClientDashboard = memo(({ handleLogout }) => {
                     type="submit"
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-sm"
                   >
-                    Submit Dog Info
+                    Ajouter un profil chien
                   </button>
                 </div>
               </form>
