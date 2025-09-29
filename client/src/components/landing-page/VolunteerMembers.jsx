03e63eb2fd5fa932d555238a2c0bd8af7cfb24f1
Julius Peschard <juliuspeschard@MacBook-Air-de-Julius.local>
2025-09-29 21:25:15 +0200

    Remove static volunteers on landing page

--- client/src/components/landing-page/VolunteerMembers.jsx
+++ client/src/components/landing-page/VolunteerMembers.jsx
@@ -1,14 +1,3 @@
-import ben1 from "../../assets/landing-page/icons/ben1-pfp.png";
-import ben3 from "../../assets/landing-page/icons/ben3-pfp.png";
-import ben4 from "../../assets/landing-page/icons/ben4-pfp.png";
-import ben5 from "../../assets/landing-page/icons/ben5-pfp.png";
-import ben6 from "../../assets/landing-page/icons/ben6-pfp.jpg";
-import ben7 from "../../assets/landing-page/icons/ben7-pfp.png";
-import ben8 from "../../assets/landing-page/icons/ben8-pfp.png";
-import ben9 from "../../assets/landing-page/icons/ben9-pfp.png";
-import ben10 from "../../assets/landing-page/icons/ben10-pfp.png";
-import ben11 from "../../assets/landing-page/icons/ben11-pfp.png";
-import ben12 from "../../assets/landing-page/icons/ben12-pfp.png";
 import julius from "../../assets/landing-page/icons/julius-pfp.png";
 import lilou from "../../assets/landing-page/icons/lilou-pfp.png";
 import nana from "../../assets/landing-page/icons/nana-pfp.png";
@@ -25,17 +14,6 @@ const VolunteerMembers = () => {
     julius,
     lilou,
     nath,
-    ben1,
-    ben3,
-    ben4,
-    ben5,
-    ben6,
-    ben7,
-    ben8,
-    ben9,
-    ben10,
-    ben11,
-    ben12,
   ];
 
   if (loading) {
